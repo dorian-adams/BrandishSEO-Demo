5251d7d47e287d3030806b32f5caf52524314f0d
dorian-adams <dorian.adams@dacoded.com>
2025-07-23 20:22:54 -0400

    Refactored authentication UI
    Improved authentication UI by implementing base_landing template.
    Added messages to base_landing.
    Updated `ResetPasswordConfirmView` to redirect to `reset_password`
    on error, for smoother UX. This was accomplished by slightly modifying `dispatch`,
    as the expected behavior would not be possible with a `super` call.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordChangeForm
@@ -9,9 +10,13 @@ from django.contrib.auth.views import (
     RedirectURLMixin,
 )
 from django.contrib.messages.views import SuccessMessageMixin
+from django.core.exceptions import ImproperlyConfigured
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+from django.views.decorators.debug import sensitive_post_parameters
 from django.views.generic import CreateView, UpdateView
 
 from checkout.models import Order
@@ -70,7 +75,47 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
 class ResetPasswordConfirmView(SuccessMessageMixin, PasswordResetConfirmView):
     template_name = "accounts/password_reset_confirm.html"
     success_message = "Password updated successfully."
-    success_url = reverse_lazy("accounts:profile")
+    success_url = reverse_lazy("accounts:login")
+
+    @method_decorator(sensitive_post_parameters())
+    @method_decorator(never_cache)
+    def dispatch(self, *args, **kwargs):
+        INTERNAL_RESET_SESSION_TOKEN = "_password_reset_token"
+
+        if "uidb64" not in kwargs or "token" not in kwargs:
+            raise ImproperlyConfigured(
+                "The URL path must contain 'uidb64' and 'token' parameters."
+            )
+        self.validlink = False
+        self.user = self.get_user(kwargs["uidb64"])
+        if self.user is not None:
+            token = kwargs["token"]
+            if token == self.reset_url_token:
+                session_token = self.request.session.get(
+                    INTERNAL_RESET_SESSION_TOKEN
+                )
+                if self.token_generator.check_token(self.user, session_token):
+                    # If the token is valid, display the password reset form.
+                    self.validlink = True
+                    return super().dispatch(*args, **kwargs)
+            else:
+                if self.token_generator.check_token(self.user, token):
+                    # Store the token in the session and redirect to the
+                    # password reset form at a URL without the token. That
+                    # avoids the possibility of leaking the token in the
+                    # HTTP Referer header.
+                    self.request.session[INTERNAL_RESET_SESSION_TOKEN] = token
+                    redirect_url = self.request.path.replace(
+                        token, self.reset_url_token
+                    )
+                    return HttpResponseRedirect(redirect_url)
+        # Display error message and redirect
+        messages.error(
+            self.request,
+            "The password reset link was invalid. Please submit a new "
+            "password reset request by completing the form below.",
+        )
+        return redirect("accounts:reset_password")
 
 
 @login_required()
